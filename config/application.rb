14d41d515a3f0de2c4d3ea274d6182ae21d0fe4e
sho ebisui <raphi.jus1412@gmail.com>
2022-12-12 17:28:12 +0900

    タイムゾーンの設定をした

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module MaprosApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
