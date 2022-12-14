a878beb1c18910c9cc2c750322ed5b21af7069cc
sho ebisui <raphi.jus1412@gmail.com>
2022-12-14 10:42:14 +0900

    ログイン時に入力した名前がマイページに出るようになった

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
   resources :dangerous_spots
-  root 'dangerous_spots#index'
+  resources :users, only: [:show]
+  root 'top#index'
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
