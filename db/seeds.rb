f43a1bc50803eb6c05b219f807f666e0cb70e1a2
sho ebisui <raphi.jus1412@gmail.com>
2022-12-19 03:14:53 +0900

    デザイン面を進ませることができた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               )
+end
+
+Label.create!(name: "歩行")
+Label.create!(name: "車")
+Label.create!(name: "自動車")
