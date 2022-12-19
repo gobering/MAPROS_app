25ea42d701fe1cf5fc0b20c41f3d98b0d8eb2766
sho ebisui <raphi.jus1412@gmail.com>
2022-12-20 00:26:05 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.16.0'
 
 # デプロイするアプリケーション名
-set :application, 'MAPROS_app'
+set :application, 'mapros_app'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server '35.77.180.206', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/ebisuisho/.ssh/id_rsa'
