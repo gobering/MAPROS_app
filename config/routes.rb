f8d30bfb31c06613a5e5b0e829fcb1460c50b4fe
sho ebisui <raphi.jus1412@gmail.com>
2022-12-20 11:44:54 +0900

    rootの設定を１番上にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'top#index'
   resources :labels
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
   }
   resources :dangerous_spots
   resources :users, only: [:show] 
-  root 'top#index'
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
