c2c74f16839d69f7cfe64f5f26d7a63c0a38aa89
sho ebisui <raphi.jus1412@gmail.com>
2022-12-20 23:36:04 +0900

    ラベルモデルのテストを作成

---
+++ spec/models/label_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+describe 'ラベルモデル機能', type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ラベルを作成' do
+    it '作成した内容が反映される' do
+      label = FactoryBot.build(:label)
+      expect(label).to be_valid
+    end
+  end
+  context 'ラベル名が空' do
+    it 'バリデーションに引っかかる' do
+      label = FactoryBot.build(:label, name: nil)
+      expect(label).not_to be_valid
+    end
+  end
+end
